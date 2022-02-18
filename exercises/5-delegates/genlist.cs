4ecddf70f9f4dba32719b31f238df72aafa8a9e8
Nikolaj Roager Christensen <nikolaj@ulvedalen.dk>
2022-02-18 13:59:45 +0100

    Exercise 5

--- exercises/5-delegates/genlist.cs
+++ exercises/5-delegates/genlist.cs
@@ -1,4 +1,22 @@
 public class genlist<T>
-{//A generic type T
+{
+//A generic list of type T wit hdynamic size
+    public T[] data;//Public type array
+    public genlist()
+    {
+        data = new T[0];
+    }
+
+    //Specify where we push
+    public void push_back(T item)
+    {
+        int n = data.Length;
+        T[] newdata = new T[n+1];
+
+        for (int i = 0; i<n; ++i)
+            newdata[i]=data[i];
+        data=newdata;//Move by reference, old data will be picked up by the garbage
+        data[n]=item;
+    }
 
 }
---
+++ exercises/5-delegates/main.cs
@@ -0,0 +1,21 @@
+using System;
+using static System.Console;
+
+class main
+{
+    public static void Main()
+    {
+        WriteLine("A");
+        genlist<int> list = new genlist<int>();
+        WriteLine("B");
+        /*
+        list.push_back(0);
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+        WriteLine("C");
+        for (int i = 0; i<list.data.Length; ++i)
+            WriteLine($"data[{i}]={i}");
+        */
+    }
+}
