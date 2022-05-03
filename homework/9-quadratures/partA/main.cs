54183c8d747588a795aaeb8c9de822b2613c022f
Nikolaj Roager Christensen <nikolaj@debian.localhost>
2022-05-03 09:48:22 +0200

    changes to output, and list of missing exercises

--- homework/9-quadratures/partA/main.cs
+++ homework/9-quadratures/partA/main.cs
@@ -19,21 +19,24 @@ public static class main
 
     public static int Main()
     {
-
-        double res0 = integrate(Sqrt,0,1);
-        WriteLine($"integrate(Sqrt,0,1)={res0}"+(approx(res0,2.0/3.0) ? " PASS" : " FAIL"));//Just use the already existing squareroot function
+        double res0,Err0;
+        (res0,Err0) = integrate(Sqrt,0,1);
+        WriteLine($"integrate(Sqrt,0,1)={res0} ± {Err0}"+(approx(res0,2.0/3.0,Err0,Err0) ? " PASS" : " FAIL"));//Just use the already existing squareroot function
 
         Func<double,double> invSqrt = x  => 1.0/Sqrt(x);
-        double res1 = integrate(invSqrt,0,1);
-        WriteLine($"integrate(1/Sqrt,0,1)={res1}"+(approx(res1,2.0) ? " PASS" : " FAIL"));
+        double res1,Err1;
+        (res1,Err1) = integrate(invSqrt,0,1);
+        WriteLine($"integrate(1/Sqrt,0,1)={res1} ± {Err1}"+(approx(res1,2.0,Err1,Err1) ? " PASS" : " FAIL"));
 
         Func<double,double> F3 = x  => 4*Sqrt(1-x*x);
-        double res2 = integrate(F3,0,1);
-        WriteLine($"integrate(4 Sqrt(1-x^2),0,1)={res2}"+(approx(res2,PI) ? " PASS" : " FAIL"));
+        double res2,Err2;
+        (res2,Err2) = integrate(F3,0,1);
+        WriteLine($"integrate(4 Sqrt(1-x^2),0,1)={res2} ± {Err2}"+(approx(res2,PI,Err2,Err2) ? " PASS" : " FAIL"));
 
         Func<double,double> F4 = x  => Log(x)/Sqrt(x);
-        double res3 = integrate(F4,0,1);
-        WriteLine($"integrate(ln(x)/Sqrt,0,1)={res3}"+(approx(res3,-4.0) ? " PASS" : " FAIL"));
+        double res3,Err3;
+        (res3,Err3) = integrate(F4,0,1);
+        WriteLine($"integrate(ln(x)/Sqrt,0,1)={res3} ± {Err3} "+(approx(res3,-4.0,Err3,Err3) ? " PASS" : " FAIL"));
 
         //Error function estimate
 
--- homework/9-quadratures/partA/quad.cs
+++ homework/9-quadratures/partA/quad.cs
@@ -4,7 +4,7 @@ using static System.Math;
 
 public static class quad
 {
-    public static double integrate(Func <double,double> F, double x_min, double x_max, double abs_acc=1e-9, double rel_acc=1e-9, double point_1=double.NaN, double point_2=double.NaN)
+    public static (double,double) integrate(Func <double,double> F, double x_min, double x_max, double abs_acc=1e-9, double rel_acc=1e-9, double point_1=double.NaN, double point_2=double.NaN)
     {
         //Step size
         double h=x_max-x_min;
@@ -25,16 +25,21 @@ public static class quad
 
         //If within error return
         if (err <= abs_acc+rel_acc*Abs(Q))
-            return Q;
+            return (Q,err);
 
         //Otherwise integrate the two half-intervals, notably the old 1/6 and 2/6 way points are now the new 2/6 and 4/6 points for the first halves, and the old 4/6 and 5/6 points are now the new 2/6 and 4/6 points:
 
  //Old points  I   I        I   I
           //-I-I---I-I----I-I---I-I-
 //New points I I   I I    I I   I I
-        else return integrate(F,x_min,(x_max+x_min)/2,abs_acc/Sqrt(2),rel_acc,point_0,point_1)+
-                    integrate(F,(x_min+x_max)/2,x_max,abs_acc/Sqrt(2),rel_acc,point_2,point_3);
-        return 0;
+        else
+        {
+            double Q1, E1, Q2, E2;
+            (Q1,E1) =integrate(F,x_min,(x_max+x_min)/2,abs_acc/Sqrt(2),rel_acc,point_0,point_1);
+            (Q2,E2) = integrate(F,(x_min+x_max)/2,x_max,abs_acc/Sqrt(2),rel_acc,point_2,point_3);
+
+            return (Q1+Q2,Sqrt(E1*E1+E2*E2));
+        }
     }
 
 }
